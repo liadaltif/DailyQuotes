bf7973274a3c73f3250b50550553f6de20c04264
Liad Altif <liadaltif@gmail.com>
2025-08-12 18:08:33 +0300

    fix

--- DailyQuotesWidget/WidgetView.swift
+++ DailyQuotesWidget/WidgetView.swift
@@ -8,22 +8,27 @@ struct DailyQuotesWidgetView: View {
         ZStack {
             if let url = entry.backgroundURL,
                let uiImage = UIImage(contentsOfFile: url.path) {
-                if #available(iOSApplicationExtension 17.0, *) {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFill()
-                        .clipped()
-                        .containerBackground(for: .widget) { Color.clear }
-                } else {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFill()
-                        .clipped()
-                }
+               Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
             } else {
                 Color.black.opacity(0.2)
             }
         }
         .ignoresSafeArea()
+        .applyWidgetBackground()
+    }
+}
+
+private extension View {
+    @ViewBuilder
+    func applyWidgetBackground() -> some View {
+        if #available(iOSApplicationExtension 17.0, *) {
+            self.containerBackground(for: .widget) { Color.clear }
+        } else {
+            self
+        }
+
     }
 }
